7a28a549e947a29f7bf16f9c88ec6132fd423051
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-12-09 15:52:42 +0100

    Header and Carousel done

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
 const gulp = require('gulp');
-const { watch, src, dest } = require('gulp');
+const { watch, src, dest, parallel } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const CompSass = require('gulp-sass');
 
 /* Watchers per al scss */
 function sass(){
-    return src('./src/sass/**/*.scss')
+    return src('./public_html/sass/**/*.scss')
         .pipe(CompSass())
-        .pipe(dest('css/'))
-}
-
-function watcherSass() {
-    watch('./src/sass/**/*.scss', sass);
+        .pipe(dest('./public_html/css/'))
 }
 
 /* Funció o tarea per a minimitzar el css */
 function minifyCSS(){
-    return src('./src/css/*.css')
+    return src('./public_html/css/*.css')
         .pipe(cleanCSS())
-        .pipe(dest('dist/css/'));
+        .pipe(dest('public_html/css/'));
 }
 
+function watcherSass() {
+    watch('./public_html/sass/**/*.scss', sass);
+}
+
+function watcherCSS(){
+    watch('./public_html/css/**/*.css', minifyCSS);
+}
+
+// Tasques
 exports.sass = sass;
 exports.watcher = watcherSass;
 exports.minCSS = minifyCSS;
